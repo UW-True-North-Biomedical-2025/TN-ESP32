7dd48a9cca08875a4186f8248bc47888c49ac5fa
halaalzu <halzureiqi@gmail.com>
2026-01-13 21:12:31 -0500

    Initial Set-UP

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+char data = 0;    
+bool status = false;        //Variable for storing received data
+void  setup()
+{
+    Serial.begin(9600);   //Sets the baud for serial data transmission                               
+    pinMode(2, OUTPUT);  //Sets digital pin  13 as output pin
+}
+void loop()
+{
+//    if(Serial.available() > 0)      //  Send data only when you receive data:
+//    {
+//       data = Serial.read();        //Read  the incoming data & store into data
+//       Serial.print(data);          //Print  Value inside data in Serial monitor
+//       Serial.print("\
+// ");        
+      if(status == true)              // Checks whether value of data is equal to 1
+         digitalWrite(2, HIGH);   //If value is 1 then LED turns ON
+      else  if(status == false)         //  Checks whether value of data is equal to 0
+         digitalWrite(2,  LOW);    //If value is 0 then LED turns OFF
+   }
