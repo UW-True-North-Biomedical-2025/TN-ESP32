6695569dfc5ee85aa69223d223f94559ac6e888d
nzou <nancy.zou@constantcontact.com>
2026-02-24 19:48:20 -0500

    updated to esp32-c3 instead of esp32

--- include/i2c_esp32.h
+++ include/i2c_esp32.h
@@ -1,12 +1,9 @@
-// declarations
-
 #ifndef ESP32_I2C_H
 #define ESP32_I2C_H
 
 #include <Arduino.h>
 #include <stdint.h>
 
-// functions
 void i2c_init(uint8_t addr = 0x08);
 bool i2c_has_new_data();
 uint8_t i2c_get_last_value();
