8a6ec45e882790bbfc2ac259988cd0a29809b5f7
nzou <nancy.zou@constantcontact.com>
2026-03-03 19:35:12 -0500

    editing comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,3 @@
-// #include <Arduino.h>
-
-// char data = 0;
-// bool status = false;        //Variable for storing received data
-// void  setup()
-// {
-//     Serial.begin(115200);   //Sets the baud for serial data transmission
-//     delay(1000);          //Wait for serial connection to establish
-
-//     // Test if ESP is connected
-//     Serial.println("ESP32 Connected and Ready!");
-//     Serial.println("Waiting for commands...");
-//     Serial.println("Send '1' to turn LED ON, '0' to turn LED OFF");
-
-//     pinMode(2, OUTPUT);  //Sets digital pin  13 as output pin
-
-//     // Blink LED to indicate successful connection
-//     for(int i = 0; i < 3; i++) {
-//         digitalWrite(2, HIGH);
-//         delay(200);
-//         digitalWrite(2, LOW);
-//         delay(200);
-//     }
-// }
-// void loop() {
-//     if(Serial.available() > 0) {
-//         data = Serial.read();
-//         if(data == '1')
-//             digitalWrite(2, HIGH);
-//         else if(data == '0')
-//             digitalWrite(2, LOW);
-//     }
-// }
-
-// //    if(Serial.available() > 0)      //  Send data only when you receive data:
-// //
-// //       data = Serial.read();        //Read  the incoming data & store into data
-// //       Serial.print(data);          //Print  Value inside data in Serial monitor
-// //       Serial.print("\
-// // ");
-//       // if(status == true)              // Checks whether value of data is equal to 1
-//       //    digitalWrite(2, HIGH);   //If value is 1 then LED turns ON
-//       // else  if(status == false)         //  Checks whether value of data is equal to 0
-//       //    digitalWrite(2,  LOW);    //If value is 0 then LED turns OFF
-
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -50,11 +5,9 @@
 #include "i2c_esp32.h"
 #include <BLE2902.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 // UUIDs for BLE service and characteristics
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define EMG1_CHAR_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -90,7 +43,7 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
-  i2c_init(0x08); // Initialize I2C with address 0x08
+  i2c_init(0x08);
 
   BLEDevice::init("MyESP32");
   BLEServer *pServer = BLEDevice::createServer();
@@ -133,6 +86,7 @@ void setup()
 
 void loop()
 {
+  // Check for new I2C data and print it (currently just a counter)
   if (i2c_has_new_data())
   {
     Serial.print("Received value: ");
